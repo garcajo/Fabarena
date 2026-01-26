39dea2167d3d440f3c9e24dd7a10b7396d083cde
garcajo <garcajo.ttt@gmail.com>
2026-01-26 00:12:02 +0000

    Fix: Move vite to dependencies to fix build error

--- fab-tcg-web/src/main.jsx
+++ fab-tcg-web/src/main.jsx
@@ -3,6 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// AGGRESSIVE CACHE CLEARING
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      console.log('Unregistering SW:', registration);
+      registration.unregister();
+    }
+    if (registrations.length > 0) {
+      console.log('Reloading to clear cache...');
+      window.location.reload();
+    }
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
